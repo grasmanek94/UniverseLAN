bbf1b1b4e9c3c0b8bdbf9fde9cc3278fcf5605b1
grasmanek94 <github@gz0.nl>
2022-09-16 01:02:29 +0200

    Don't update version when it doesn't change

--- Common/Version/auto_version.h
+++ Common/Version/auto_version.h
@@ -1,1 +1,1 @@
-#define BUILD_VERSION 28 
+#define BUILD_VERSION 29 
--- Util/UpdateVersion.bat
+++ Util/UpdateVersion.bat
@@ -1,10 +1,24 @@
 @ECHO OFF
 cd /d "%~dp0"
 cd ../Common/Version
-copy /b Version.cxx +,,
-echo touched version.cxx ...
+
 for /f "delims=" %%a in ('git rev-list HEAD --count') do set _COUNT=%%a
-echo #define BUILD_VERSION %_COUNT% > auto_version.h
-echo Updated version to %_COUNT%
-cd /d %~dp0
+echo #define BUILD_VERSION %_COUNT% > auto_version_check.h
+
+fc /b auto_version.h auto_version_check.h > nul
+if errorlevel 1 (
+    echo Updating version...
+    copy /b Version.cxx +,,
+    echo touched version.cxx ...
+    del auto_version.h
+    move auto_version_check.h auto_version.h
+    echo Updated version to %_COUNT%
+    cd /d %~dp0
 REM exit 0
+) else (
+    del auto_version_check.h
+    echo Version doesn't need updating
+)
+
+ver > nul
+type nul>nul
