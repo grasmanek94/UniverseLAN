0d3b6130a5639c34677ce05168e8066ddf34f11a
grasmanek94 <github@gz0.nl>
2023-09-13 10:19:03 +0200

    Remove dead messaged in cmake

--- Source/Client/Impl/InitOptionsModern.hxx
+++ Source/Client/Impl/InitOptionsModern.hxx
@@ -1,53 +1,7 @@
-#ifndef LSFDRMFG_IMPL_INIT_OPTIONS_H
-#define LSFDRMFG_IMPL_INIT_OPTIONS_H
+#pragma once
 
 #if GALAXY_BUILD_FEATURE_FACTORY_INTERFACE
-#include <stdint.h>
-#include <cstddef>
-
-namespace galaxy
-{
-	namespace api
-	{
-		/**
-		 * @addtogroup api
-		 * @{
-		 */
-
-		 /**
-		 * The group of options used for Init configuration.
-		 */
-		struct InitOptions
-		{
-			/**
-			 * InitOptions constructor.
-			 *
-			 * @remark On Android device Galaxy SDK needs a directory for internal storage (to keep a copy of system CA certificates).
-			 * It should be an existing directory within the application file storage.
-			 *
-			 * @param [in] _clientID The ID of the client.
-			 * @param [in] _clientSecret The secret of the client.
-			 * @param [in] _configFilePath The path to folder which contains configuration files.
-			 * @param [in] _galaxyAllocator The custom memory allocator. Shall only be set once for both Galaxy Peer and Game Server.
-			 * @param [in] _storagePath Path to a directory that can be used for storing internal SDK data. Used only on Android devices. See remarks for more information.
-			 */
-			InitOptions(const char* _clientID, const char* _clientSecret, const char* _configFilePath = ".", void* _galaxyAllocator = NULL, const char* _storagePath = NULL)
-				: clientID(_clientID)
-				, clientSecret(_clientSecret)
-				, configFilePath(_configFilePath)
-				, storagePath(_storagePath)
-			{
-			}
-
-			const char* clientID; ///< The ID of the client.
-			const char* clientSecret; ///< The secret of the client.
-			const char* configFilePath; ///< The path to folder which contains configuration files.
-			const char* storagePath; ///< The path to folder for storing internal SDK data. Used only on Android devices.
-		};
-
-		/** @} */
-	}
-}
+#include "InitOptionsFactory.hxx"
 #else
 #include <InitOptions.h>
 #endif
@@ -78,7 +32,8 @@ namespace universelan::client {
 		std::string clientSecret; ///< The secret of the client.
 		std::string configFilePath; ///< The path to folder which contains configuration files.
 		std::string storagePath; ///< The path to folder for storing internal SDK data. Used only on Android devices.
-		GalaxyAllocator* galaxyAllocator; ///< The custom memory allocator used by GOG Galaxy SDK.
+		GalaxyAllocator* galaxyAllocator;
+
 #if (GALAXY_VERSION) > 112400
 		IGalaxyThreadFactory* galaxyThreadFactory; ///< The custom thread factory used by GOG Galaxy SDK to spawn internal threads.
 		std::string host; ///< The local IP address this peer would bind to.
@@ -88,5 +43,3 @@ namespace universelan::client {
 
 	/** @} */
 }
-
-#endif
