fb793c4d22e4aeecc366bb66f5c97b46cab10d71
grasmanek94 <github@gz0.nl>
2023-09-17 23:44:36 +0200

    Linux also add versions

--- cmake-linux.sh
+++ cmake-linux.sh
@@ -5,4 +5,13 @@ mkdir cmake-linux/
 cd cmake-linux/
 
 # GCC 13 or 14
-cmake -D CMAKE_C_COMPILER=/usr/local/bin/gcc -D CMAKE_CXX_COMPILER=/usr/local/bin/g++ ..
+GALAXY_ARCH="x$(getconf LONG_BIT)"
+
+if [ "$GALAXY_ARCH" = "x64" ]; then
+    echo "Using GALAXY_ARCH=x64"
+else
+    echo "Using GALAXY_ARCH=x86"
+    GALAXY_ARCH="x86"
+fi
+
+cmake -D CMAKE_C_COMPILER=/usr/local/bin/gcc -D CMAKE_CXX_COMPILER=/usr/local/bin/g++ -D GALAXY_ARCH="${GALAXY_ARCH}" ..
