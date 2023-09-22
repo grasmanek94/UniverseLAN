d7779c6a76839f7b2cdeb6026c4efc332a2a70ae
grasmanek94 <github@gz0.nl>
2023-09-22 20:22:56 +0200

    Create PowerShell script to create release packages for GitHub

---
+++ package-release.ps1
@@ -0,0 +1,38 @@
+$subdirs = Get-ChildItem -Directory .\bin\
+$build_number = $(git rev-list HEAD --count)
+$package_folder = "Release"
+$output_folder = "release-packages"
+$prefix = "UniverseLAN"
+
+echo $build_number
+
+foreach ($version in $subdirs)
+{
+    if($version.Name -ne "Debug" -and $version.Name -ne "Release")
+    {
+        $version_release_dir = ".\bin\" + $version.Name + "\$package_folder"
+
+        $exists_x86 = Test-Path -Path "$version_release_dir\UniverseLANServer.exe" -PathType Leaf
+        $exists_x64 = Test-Path -Path "$version_release_dir\UniverseLANServer64.exe" -PathType Leaf
+
+        $ext = ''
+        if($exists_x86 -and $exists_x64) {
+            $ext =  "x64_x86"
+        } elseif($exists_x86) {
+            $ext = "x86"
+        } elseif($exists_x64) {
+            $ext = "x64"
+        }
+
+        if($ext) {
+            $resulting_filename =  "$prefix-$version-Build-$build_number-$ext.zip"
+
+            if(-not(Test-Path ".\$output_folder"))
+            {
+                New-Item -Path ".\$output_folder" -ItemType Directory -Force > $null
+            }
+
+            Compress-Archive -Path "$version_release_dir\*" -DestinationPath ".\$output_folder\$resulting_filename"
+        }
+    }
+}
