32a53568facf34736794b492160b940a9005a2fa
grasmanek94 <github@gz0.nl>
2024-05-02 15:57:42 +0200

    Fix out of bounds possible access in container_get_by_index by providing a too large index.

--- Source/Shared/ContainerGetByIndex.hxx
+++ Source/Shared/ContainerGetByIndex.hxx
@@ -3,11 +3,11 @@
 #include <iterator>
 
 namespace universelan {
-	template <typename T> auto container_get_by_index(const T& container, size_t index) {
-		auto it = container.begin();
-		while (index--) {
-			++it;
+	template <typename T> auto container_get_by_index(const T& container, size_t index, const typename T::value_type& default_not_found_value = T::value_type()) {
+		if (index < container.size()) {
+			return *std::next(container.begin(), index);
 		}
-		return *it;
+
+		return T::value_type
 	}
 }
