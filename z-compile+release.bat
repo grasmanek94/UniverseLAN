a0f4dbb9e6f1c92a0eca9b488178e2ee1d782197
grasmanek94 <github@gz0.nl>
2024-05-14 00:00:00 +0200

    add multiple threads

--- z-compile+release.bat
+++ z-compile+release.bat
@@ -12,7 +12,7 @@ mkdir release-packages
 cmake -A x64 -B cmake-x64
 cmake -A Win32 -B cmake-x86
 
-cmake --build cmake-x64 --config Release
-cmake --build cmake-x86 --config Release
+cmake --build cmake-x64 --config Release -j 24
+cmake --build cmake-x86 --config Release -j 24
 
 powershell -File ".\package-release.ps1"
