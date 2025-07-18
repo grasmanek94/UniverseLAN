fd42bae63abc24f2f0f02382a82555ca160f121f
grasmanek94 <github@gz0.nl>
2025-07-18 21:10:58 +0200

    Generic parser progress

--- UniverseLanLogAnalyzer/Parser/PropertyParser.cs
+++ UniverseLanLogAnalyzer/Parser/PropertyParser.cs
@@ -6,7 +6,7 @@ namespace UniverseLanLogAnalyzer.Parser
     {
         private static readonly Regex PlaceholderRegex = new(@"\{(d|x|e|ef|b|s)(\?([a-zA-Z_][a-zA-Z0-9_]*)==(.+?))?\}");
 
-        public static bool Parse(ref string[] properties, string template, params object[] outputs)
+        public static bool Parse(ref string[] properties, string template, object[] outputs, Type[] types)
         {
             var lines = template.Split('\n');
             var matchedIndices = new HashSet<int>();
@@ -15,9 +15,8 @@ namespace UniverseLanLogAnalyzer.Parser
 
             foreach (var line in lines)
             {
-                var pattern = Regex.Escape(line);
                 var placeholders = new List<(string type, string? condVar, string? condVal)>();
-
+                string pattern = line;
                 pattern = PlaceholderRegex.Replace(pattern, match =>
                 {
                     placeholders.Add((
@@ -27,6 +26,8 @@ namespace UniverseLanLogAnalyzer.Parser
                     ));
                     return "(.+?)";
                 });
+                pattern = Regex.Escape(pattern);
+                pattern = pattern.Replace(Regex.Escape("(.+?)"), "(.+?)");
 
                 var regex = new Regex("^" + pattern + "$");
 
@@ -74,8 +75,8 @@ namespace UniverseLanLogAnalyzer.Parser
                             "d" => ulong.Parse(value),
                             "x" => Convert.ToUInt64(value.StartsWith("0x") ? value[2..] : value, 16),
                             "b" => ParseBool(value),
-                            "e" => ParseEnum(outputs[outputIndex].GetType(), value),
-                            "ef" => ParseEnumFlags(outputs[outputIndex].GetType(), value),
+                            "e" => ParseEnum(types[outputIndex], value),
+                            "ef" => ParseEnumFlags(types[outputIndex], value),
                             "s" => value,
                             _ => throw new NotSupportedException($"Unsupported placeholder: {type}")
                         };
@@ -134,16 +135,20 @@ namespace UniverseLanLogAnalyzer.Parser
 
         public static bool Parse<T1>(ref string[] properties, string template, out T1 arg1)
         {
+            Type[] types = new[] { typeof(T1) };
+
             object[] outputs = new object[1];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             return result;
         }
 
         public static bool Parse<T1, T2>(ref string[] properties, string template, out T1 arg1, out T2 arg2)
         {
+            Type[] types = new[] { typeof(T1), typeof(T2) };
+
             object[] outputs = new object[2];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             arg2 = (T2)outputs[1]!;
             return result;
@@ -151,8 +156,10 @@ namespace UniverseLanLogAnalyzer.Parser
 
         public static bool Parse<T1, T2, T3>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3)
         {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3) };
+
             object[] outputs = new object[3];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             arg2 = (T2)outputs[1]!;
             arg3 = (T3)outputs[2]!;
@@ -161,8 +168,10 @@ namespace UniverseLanLogAnalyzer.Parser
 
         public static bool Parse<T1, T2, T3, T4>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3, out T4 arg4)
         {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4) };
+
             object[] outputs = new object[4];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             arg2 = (T2)outputs[1]!;
             arg3 = (T3)outputs[2]!;
@@ -172,8 +181,10 @@ namespace UniverseLanLogAnalyzer.Parser
 
         public static bool Parse<T1, T2, T3, T4, T5>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3, out T4 arg4, out T5 arg5)
         {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5) };
+
             object[] outputs = new object[5];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             arg2 = (T2)outputs[1]!;
             arg3 = (T3)outputs[2]!;
@@ -184,14 +195,51 @@ namespace UniverseLanLogAnalyzer.Parser
 
         public static bool Parse<T1, T2, T3, T4, T5, T6>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3, out T4 arg4, out T5 arg5, out T6 arg6)
         {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6) };
+
             object[] outputs = new object[6];
-            bool result = Parse(ref properties, template, outputs);
+            bool result = Parse(ref properties, template, outputs, types);
             arg1 = (T1)outputs[0]!;
             arg2 = (T2)outputs[1]!;
             arg3 = (T3)outputs[2]!;
             arg4 = (T4)outputs[3]!;
             arg5 = (T5)outputs[4]!;
             arg6 = (T6)outputs[5]!;
+            return result;
+        }
+
+        public static bool Parse<T1, T2, T3, T4, T5, T6, T7>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3, out T4 arg4, out T5 arg5, out T6 arg6, out T7 arg7)
+        {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7) };
+
+            object[] outputs = new object[7];
+            bool result = Parse(ref properties, template, outputs, types);
+            arg1 = (T1)outputs[0]!;
+            arg2 = (T2)outputs[1]!;
+            arg3 = (T3)outputs[2]!;
+            arg4 = (T4)outputs[3]!;
+            arg5 = (T5)outputs[4]!;
+            arg6 = (T6)outputs[5]!;
+            arg7 = (T7)outputs[6]!;
+
+            return result;
+        }
+
+        public static bool Parse<T1, T2, T3, T4, T5, T6, T7, T8>(ref string[] properties, string template, out T1 arg1, out T2 arg2, out T3 arg3, out T4 arg4, out T5 arg5, out T6 arg6, out T7 arg7, out T8 arg8)
+        {
+            Type[] types = new[] { typeof(T1), typeof(T2), typeof(T3), typeof(T4), typeof(T5), typeof(T6), typeof(T7), typeof(T8) };
+
+            object[] outputs = new object[8];
+            bool result = Parse(ref properties, template, outputs, types);
+            arg1 = (T1)outputs[0]!;
+            arg2 = (T2)outputs[1]!;
+            arg3 = (T3)outputs[2]!;
+            arg4 = (T4)outputs[3]!;
+            arg5 = (T5)outputs[4]!;
+            arg6 = (T6)outputs[5]!;
+            arg7 = (T7)outputs[6]!;
+            arg8 = (T8)outputs[7]!;
+
             return result;
         }
     }
