14f9cb45c6b217a9930e6fd68177b6ce8c7415b4
grasmanek94 <github@gz0.nl>
2025-07-18 13:07:02 +0200

    Update simpleini and cereal

--- Source/Vendor/cereal.cmake
+++ Source/Vendor/cereal.cmake
@@ -1,8 +1,8 @@
 cmake_minimum_required(VERSION 3.12)
 
-file(GLOB_RECURSE CEREAL_SRC CONFIGURE_DEPENDS "cereal-1.1.2/include/*.*")
+file(GLOB_RECURSE CEREAL_SRC CONFIGURE_DEPENDS "cereal-1.3.2/include/*.*")
 
 add_library(universelan-cereal STATIC ${CEREAL_SRC})
 target_sources(universelan-cereal PRIVATE dummy.c)
 
-target_include_directories(universelan-cereal PUBLIC "cereal-1.1.2/include/")
+target_include_directories(universelan-cereal PUBLIC "cereal-1.3.2/include/")
--- Source/Vendor/simpleini.cmake
+++ Source/Vendor/simpleini.cmake
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.12)
 
-add_library(universelan-simpleini STATIC "simpleini-4.19/ConvertUTF.c" "simpleini-4.19/ConvertUTF.h" "simpleini-4.19/SimpleIni.h" )
+add_library(universelan-simpleini STATIC "simpleini-4.22/ConvertUTF.c" "simpleini-4.22/ConvertUTF.h" "simpleini-4.22/SimpleIni.h" )
 
-target_include_directories(universelan-simpleini PUBLIC "simpleini-4.19/")
+target_include_directories(universelan-simpleini PUBLIC "simpleini-4.22/")
