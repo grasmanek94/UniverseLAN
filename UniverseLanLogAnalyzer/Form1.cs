58f6389171236977f4180b9b1cbc794e4e07d3b9
grasmanek94 <github@gz0.nl>
2025-07-23 13:52:46 +0200

    cleanup initial test code

--- UniverseLanLogAnalyzer/Form1.cs
+++ UniverseLanLogAnalyzer/Form1.cs
@@ -5,31 +5,25 @@ namespace UniverseLanLogAnalyzer
 {
     public partial class Form1 : Form
     {
+        public static readonly string[] test_files = new string[]
+        {
+            @"C:\Users\Rafal\Desktop\SSBD-real-gog-log\Working-A.log",
+            @"C:\Users\Rafal\Desktop\SSBD-real-gog-log\Working-B.log",
+            @"C:\Users\Rafal\Desktop\SSBD-real-gog-log\NotWorking-A.log",
+            @"C:\Users\Rafal\Desktop\SSBD-real-gog-log\NotWorking-B.log"
+        };
+
         public Form1()
         {
             InitializeComponent();
 
+            foreach(var file in test_files)
             {
                 LoggerStateMachine state_machine = new LoggerStateMachine();
-                LogParser parser = new LogParser(@"C:\Users\Rafal\Desktop\SSBD-real-gog-log\Working-A.log", state_machine);
+                LogParser parser = new LogParser(file, state_machine);
                 parser.Parse();
                 LoggerStateMachineCleanup cleanup = new LoggerStateMachineCleanup(state_machine);
                 cleanup.FoldRecurring();
-            }
-            {
-                LoggerStateMachine state_machine = new LoggerStateMachine();
-                LogParser parser = new LogParser(@"C:\Users\Rafal\Desktop\SSBD-real-gog-log\Working-B.log", state_machine);
-                parser.Parse();
-            }
-            {
-                LoggerStateMachine state_machine = new LoggerStateMachine();
-                LogParser parser = new LogParser(@"C:\Users\Rafal\Desktop\SSBD-real-gog-log\NotWorking-A.log", state_machine);
-                parser.Parse();
-            }
-            {
-                LoggerStateMachine state_machine = new LoggerStateMachine();
-                LogParser parser = new LogParser(@"C:\Users\Rafal\Desktop\SSBD-real-gog-log\NotWorking-B.log", state_machine);
-                parser.Parse();
             }
         }
     }
