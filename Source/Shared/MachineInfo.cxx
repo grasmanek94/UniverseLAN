643ee6d0cf0ee59415ad2ed73f7b32fe2b795b1f
grasmanek94 <github@gz0.nl>
2025-12-29 13:00:55 +0100

    Refactor machine info, add more constness

--- Source/Shared/MachineInfo.cxx
+++ Source/Shared/MachineInfo.cxx
@@ -38,207 +38,79 @@ namespace universelan {
 
 		const std::wstring universelan_directory(L"UniverseLAN");
 
-		void GetCurrentLibraryPaths(MachineInfo::KnownPaths& known_paths);
-	}
-
-	MachineInfo::MachineInfo() :
-		machine_name{ "" }, user_name{ "" }, machine_id{ "" }, macs{}, known_paths{}
-	{
-		::universelan::GetCurrentLibraryPaths(known_paths);
-	}
-
-	static std::string hexStr(const uint8_t* data, int len);
-#ifdef _WIN32
-	static int RetriveLocalMacAddress(ULONG ulFlags, ULONG ulFamily, unsigned char** pszAddress);
-#endif
-
-	std::string MachineInfo::GetLocalMachineName()
-	{
-#ifdef _WIN32
-		if (machine_name.length() == 0) {
-			const size_t INFO_BUFFER_SIZE = 32;
-			TCHAR  infoBuf[INFO_BUFFER_SIZE];
-			DWORD  bufCharCount = INFO_BUFFER_SIZE;
-
-			// Get the computer name.
-			if (GetComputerName(infoBuf, &bufCharCount)) {
-				if (bufCharCount > 0) {
-					machine_name.assign((const char* const)infoBuf, (const size_t)(bufCharCount - 1));
-				}
-			}
-		}
-#else
-		machine_name = "GetLocalMachineNameNotImplemented";
-#endif
-
-		return machine_name;
-	}
-
-	std::string MachineInfo::GetLocalUserName()
-	{
-#ifdef _WIN32
-		if (user_name.length() == 0) {
-			const size_t INFO_BUFFER_SIZE = 256;
-			TCHAR  infoBuf[INFO_BUFFER_SIZE];
-			DWORD  bufCharCount = INFO_BUFFER_SIZE;
-
-			// Get the user name.
-			if (GetUserName(infoBuf, &bufCharCount)) {
-				if (bufCharCount > 0) {
-					user_name.assign((const char* const)infoBuf, (const size_t)(bufCharCount - 1));
-				}
-			}
-		}
-#else
-		user_name = "GetLocalUserNameNotImplemented";
-#endif
-
-		return user_name;
-	}
-
-	std::string MachineInfo::GetInstallationID()
-	{
-#ifdef _WIN32
-		const std::string subkey = "SOFTWARE\\Microsoft\\Cryptography";
-		const std::string valueName = "MachineGuid";
-		HKEY hKey = NULL;
-		char guid[256];
-		DWORD guidSize = sizeof(guid);
-
-		if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subkey.c_str(), 0, KEY_READ | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS) {
-			if (RegQueryValueExA(hKey, valueName.c_str(), nullptr, nullptr, reinterpret_cast<LPBYTE>(guid), &guidSize) == ERROR_SUCCESS) {
-				machine_id.assign((const char* const)guid, (const size_t)(guidSize - 1));
-			}
-			RegCloseKey(hKey);
-		}
-
-#else
-		std::ifstream file("/etc/machine-id");
-		if (file.is_open()) {
-			std::getline(file, machine_id);
-		}
-#endif
-
-		return machine_id;
-	}
-
-	std::vector<std::string> MachineInfo::GetLocalMACs()
-	{
-		if (macs.size() == 0) {
-#ifdef _WIN32
-			unsigned char* pszBuff = NULL;
-			int nCount = 0;
-			int idx = 0;
-			int chPos = 0;
-			nCount = RetriveLocalMacAddress(GAA_FLAG_INCLUDE_ALL_COMPARTMENTS, AF_UNSPEC, &pszBuff);
-
-			for (idx = 0; idx < nCount; ++idx)
-			{
-				chPos = MAX_ADAPTER_ADDRESS_LENGTH * idx;
-				std::string str(hexStr((pszBuff + chPos), 6));
-				if (str != "000000000000") {
-					macs.push_back(str);
-				}
-			}
-
-			HeapFree(GetProcessHeap(), 0x00, pszBuff);
-			pszBuff = NULL;
-#else
-			macs.push_back("12345678abcd");
-#endif
-		}
-
-		return macs;
-	}
-
-	std::size_t MachineInfo::GetProcessID() {
-		std::size_t process_id = 0;
-
-#ifdef _WIN32
-		process_id = (std::size_t)GetCurrentProcessId();
-#else
-		process_id = (std::size_t)::getpid();
-#endif
-
-		return process_id;
-	}
-
-	static std::string hexStr(const uint8_t* data, int len)
-	{
-		std::stringstream ss;
-		ss << std::hex;
-
-		for (int i(0); i < len; ++i)
-			ss << std::setw(2) << std::setfill('0') << (int)data[i];
-
-		return ss.str();
-	}
-
-#ifdef _WIN32
-	static int RetriveLocalMacAddress(ULONG ulFlags, ULONG ulFamily, unsigned char** pszAddress)
-	{
-		PIP_ADAPTER_ADDRESSES pCurrAddresses = NULL;
-		PIP_ADAPTER_ADDRESSES pAddresses = NULL;
-
-		int nAddressCount = 0;
-		DWORD dwRetVal = 0;
-		ULONG ulBufLen = sizeof(IP_ADAPTER_ADDRESSES);
-		HANDLE hHeap = NULL;
-
-		hHeap = GetProcessHeap();
-		pAddresses = (PIP_ADAPTER_ADDRESSES)HeapAlloc(hHeap, 0x00, ulBufLen);
-		if (pAddresses == NULL) {
-			return 0;
-		}
-
-		dwRetVal = GetAdaptersAddresses(ulFamily, ulFlags, NULL, pAddresses, &ulBufLen);
-		if (dwRetVal == ERROR_BUFFER_OVERFLOW)
-		{
-			HeapFree(hHeap, 0x00, pAddresses);
-			pAddresses = (PIP_ADAPTER_ADDRESSES)HeapAlloc(hHeap, 0x00, ulBufLen);
-		}
-
-		if (pAddresses == NULL) {
-			return 0;
-		}
-
-		dwRetVal = GetAdaptersAddresses(ulFamily, ulFlags, NULL, pAddresses, &ulBufLen);
-		if (dwRetVal == NO_ERROR)
-		{
-			pCurrAddresses = pAddresses;
-			while (pCurrAddresses)
-			{
-				pCurrAddresses = pCurrAddresses->Next;
-				++nAddressCount;
-			}
-
-			*pszAddress = (unsigned char*)HeapAlloc(hHeap, 0x00, MAX_ADAPTER_ADDRESS_LENGTH * nAddressCount);
-			pCurrAddresses = pAddresses;
-			nAddressCount = 0;
-			while (pCurrAddresses)
-			{
-				RtlCopyMemory(*pszAddress + (MAX_ADAPTER_ADDRESS_LENGTH * nAddressCount++),
-					pCurrAddresses->PhysicalAddress,
-					MAX_ADAPTER_ADDRESS_LENGTH);
-				pCurrAddresses = pCurrAddresses->Next;
-			}
-		}
-
-		if (pAddresses) {
-			HeapFree(hHeap, 0x00, pAddresses);
-			pAddresses = NULL;
-		}
-		return nAddressCount;
-	}
-#endif
-
-	namespace {
 		const std::string debug_temp_filename = ".universelanclientapimultiprocessdebugcounter.";
 		const int max_debug_id = 16;
 		int my_debug_id = -1;
 		std::mutex debug_id_mtx;
 
+		std::string HexStr(const uint8_t* data, int len)
+		{
+			std::stringstream ss;
+			ss << std::hex;
+
+			for (int i(0); i < len; ++i)
+				ss << std::setw(2) << std::setfill('0') << (int)data[i];
+
+			return ss.str();
+		}
+
 #ifdef _WIN32
+		int RetriveLocalMacAddress(ULONG ulFlags, ULONG ulFamily, unsigned char** pszAddress)
+		{
+			PIP_ADAPTER_ADDRESSES pCurrAddresses = NULL;
+			PIP_ADAPTER_ADDRESSES pAddresses = NULL;
+
+			int nAddressCount = 0;
+			DWORD dwRetVal = 0;
+			ULONG ulBufLen = sizeof(IP_ADAPTER_ADDRESSES);
+			HANDLE hHeap = NULL;
+
+			hHeap = GetProcessHeap();
+			pAddresses = (PIP_ADAPTER_ADDRESSES)HeapAlloc(hHeap, 0x00, ulBufLen);
+			if (pAddresses == NULL) {
+				return 0;
+			}
+
+			dwRetVal = GetAdaptersAddresses(ulFamily, ulFlags, NULL, pAddresses, &ulBufLen);
+			if (dwRetVal == ERROR_BUFFER_OVERFLOW)
+			{
+				HeapFree(hHeap, 0x00, pAddresses);
+				pAddresses = (PIP_ADAPTER_ADDRESSES)HeapAlloc(hHeap, 0x00, ulBufLen);
+			}
+
+			if (pAddresses == NULL) {
+				return 0;
+			}
+
+			dwRetVal = GetAdaptersAddresses(ulFamily, ulFlags, NULL, pAddresses, &ulBufLen);
+			if (dwRetVal == NO_ERROR)
+			{
+				pCurrAddresses = pAddresses;
+				while (pCurrAddresses)
+				{
+					pCurrAddresses = pCurrAddresses->Next;
+					++nAddressCount;
+				}
+
+				*pszAddress = (unsigned char*)HeapAlloc(hHeap, 0x00, MAX_ADAPTER_ADDRESS_LENGTH * nAddressCount);
+				pCurrAddresses = pAddresses;
+				nAddressCount = 0;
+				while (pCurrAddresses)
+				{
+					RtlCopyMemory(*pszAddress + (MAX_ADAPTER_ADDRESS_LENGTH * nAddressCount++),
+						pCurrAddresses->PhysicalAddress,
+						MAX_ADAPTER_ADDRESS_LENGTH);
+					pCurrAddresses = pCurrAddresses->Next;
+				}
+			}
+
+			if (pAddresses) {
+				HeapFree(hHeap, 0x00, pAddresses);
+				pAddresses = NULL;
+			}
+			return nAddressCount;
+		}
+
 		void GetModuleFileNameSafe(const HMODULE module, MachineInfo::KnownPath& known_path) {
 			known_path.available = false;
 
@@ -505,7 +377,128 @@ namespace universelan {
 		}
 	}
 
-	int MachineInfo::GetDebugID(std::string root_temp_path) {
+	MachineInfo::MachineInfo() :
+		machine_name{ "" }, user_name{ "" }, machine_id{ "" }, process_id{ 0 },
+		macs{}, known_paths{}
+	{
+		/* Get User Name */
+#ifdef _WIN32
+		if (user_name.length() == 0) {
+			const size_t INFO_BUFFER_SIZE = 256;
+			TCHAR  infoBuf[INFO_BUFFER_SIZE];
+			DWORD  bufCharCount = INFO_BUFFER_SIZE;
+
+			// Get the user name.
+			if (GetUserName(infoBuf, &bufCharCount)) {
+				if (bufCharCount > 0) {
+					user_name.assign((const char* const)infoBuf, (const size_t)(bufCharCount - 1));
+				}
+			}
+		}
+#else
+		user_name = "GetLocalUserNameNotImplemented";
+#endif
+
+		/* Get Machine Name */
+#ifdef _WIN32
+		if (machine_name.length() == 0) {
+			const size_t INFO_BUFFER_SIZE = 32;
+			TCHAR  infoBuf[INFO_BUFFER_SIZE];
+			DWORD  bufCharCount = INFO_BUFFER_SIZE;
+
+			// Get the computer name.
+			if (GetComputerName(infoBuf, &bufCharCount)) {
+				if (bufCharCount > 0) {
+					machine_name.assign((const char* const)infoBuf, (const size_t)(bufCharCount - 1));
+				}
+			}
+		}
+#else
+		machine_name = "GetLocalMachineNameNotImplemented";
+#endif
+
+		/* Get Machine ID */
+#ifdef _WIN32
+		const std::string subkey = "SOFTWARE\\Microsoft\\Cryptography";
+		const std::string valueName = "MachineGuid";
+		HKEY hKey = NULL;
+		char guid[256];
+		DWORD guidSize = sizeof(guid);
+
+		if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subkey.c_str(), 0, KEY_READ | KEY_WOW64_64KEY, &hKey) == ERROR_SUCCESS) {
+			if (RegQueryValueExA(hKey, valueName.c_str(), nullptr, nullptr, reinterpret_cast<LPBYTE>(guid), &guidSize) == ERROR_SUCCESS) {
+				machine_id.assign((const char* const)guid, (const size_t)(guidSize - 1));
+			}
+			RegCloseKey(hKey);
+		}
+
+#else
+		std::ifstream file("/etc/machine-id");
+		if (file.is_open()) {
+			std::getline(file, machine_id);
+		}
+#endif
+
+		/* Get Local MACs */
+#ifdef _WIN32
+		unsigned char* pszBuff = NULL;
+		int nCount = 0;
+		int idx = 0;
+		int chPos = 0;
+		nCount = RetriveLocalMacAddress(GAA_FLAG_INCLUDE_ALL_COMPARTMENTS, AF_UNSPEC, &pszBuff);
+
+		for (idx = 0; idx < nCount; ++idx)
+		{
+			chPos = MAX_ADAPTER_ADDRESS_LENGTH * idx;
+			std::string str(HexStr((pszBuff + chPos), 6));
+			if (str != "000000000000") {
+				macs.push_back(str);
+			}
+		}
+
+		HeapFree(GetProcessHeap(), 0x00, pszBuff);
+		pszBuff = NULL;
+#else
+		macs.push_back("12345678abcd");
+#endif
+
+		/* Get Process ID */
+#ifdef _WIN32
+		process_id = (std::size_t)GetCurrentProcessId();
+#else
+		process_id = (std::size_t)::getpid();
+#endif
+
+		/* Get Known Folder Paths */
+		::universelan::GetCurrentLibraryPaths(known_paths);
+	}
+
+	std::string MachineInfo::GetLocalMachineName() const
+	{
+		return machine_name;
+	}
+
+	std::string MachineInfo::GetLocalUserName() const
+	{
+		return user_name;
+	}
+
+	std::string MachineInfo::GetInstallationID() const
+	{
+		return machine_id;
+	}
+
+	std::vector<std::string> MachineInfo::GetLocalMACs() const
+	{
+		return macs;
+	}
+
+	std::size_t MachineInfo::GetProcessID() const {
+
+		return process_id;
+	}
+
+	int MachineInfo::GetDebugID(std::string root_temp_path) const {
 		std::scoped_lock<std::mutex> id_lock{ debug_id_mtx };
 
 		if (my_debug_id != -1) {
@@ -535,13 +528,7 @@ namespace universelan {
 		return -1;
 	}
 
-	std::filesystem::path MachineInfo::GetCurrentLibraryPath() {
-		return "";
-	}
-
-
-	std::filesystem::path MachineInfo::GetSettingsStoragePath() {
-
-		return "";
+	MachineInfo::KnownPaths MachineInfo::GetKnownPaths() const {
+		return known_paths;
 	}
 }
--- Source/Shared/MachineInfo.hxx
+++ Source/Shared/MachineInfo.hxx
@@ -26,22 +26,23 @@ namespace universelan {
 		std::string machine_name;
 		std::string user_name;
 		std::string machine_id;
+		std::size_t process_id;
 
 		std::vector<std::string> macs;
 
 		KnownPaths known_paths;
+
 	public:
 
 		MachineInfo();
 
-		std::string GetLocalMachineName();
-		std::string GetLocalUserName();
-		std::string GetInstallationID();
-		std::size_t GetProcessID();
-		std::vector<std::string> GetLocalMACs();
-		int GetDebugID(std::string root_temp_path);
+		std::string GetLocalMachineName() const;
+		std::string GetLocalUserName() const;
+		std::string GetInstallationID() const;
+		std::size_t GetProcessID() const;
+		std::vector<std::string> GetLocalMACs() const;
+		int GetDebugID(std::string root_temp_path) const;
 
-		std::filesystem::path GetCurrentLibraryPath();
-		std::filesystem::path GetSettingsStoragePath();
+		KnownPaths GetKnownPaths() const;
 	};
 }
