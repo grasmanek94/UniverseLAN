ced57f34f3d75d848ebee993ecba97c9b9d35ba2
grasmanek94 <github@gz0.nl>
2026-01-01 02:14:34 +0100

    fix formatting for ipv6

--- Source/Vendor/Enet++/include/enet6/enetpp.hxx
+++ Source/Vendor/Enet++/include/enet6/enetpp.hxx
@@ -86,12 +86,12 @@ struct std::formatter<ENetAddress> : std::formatter<std::string> {
 				ctx);
 		case ENET_ADDRESS_TYPE_IPV6:
 			return formatter<string>::format(
-				std::format("[{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}]:{:d}",
+				std::format("[{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}]:{:d}",
 					p.host.v6[0], p.host.v6[1], p.host.v6[2], p.host.v6[3], p.host.v6[4], p.host.v6[5], p.host.v6[6], p.host.v6[7], p.port),
 				ctx);
 		case ENET_ADDRESS_TYPE_ANY:
 			return formatter<string>::format(
-				std::format("IPv6:[{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}]:{:d} | IPv4:{:d}.{:d}.{:d}.{:d}:{:d}",
+				std::format("IPv6:[{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}:{:04x}]:{:d} | IPv4:{:d}.{:d}.{:d}.{:d}:{:d}",
 					p.host.v6[0], p.host.v6[1], p.host.v6[2], p.host.v6[3], p.host.v6[4], p.host.v6[5], p.host.v6[6], p.host.v6[7], p.port,
 					p.host.v4[0], p.host.v4[1], p.host.v4[2], p.host.v4[3], p.port),
 				ctx);
