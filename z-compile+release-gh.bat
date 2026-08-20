f22af34f0f45026dc457e8eab415cfe7f93a4c0c
grasmanek94 <github@gz0.nl>
2026-08-20 18:58:39 +0200

    limit versions temporarily for testing

--- z-compile+release-gh.bat
+++ z-compile+release-gh.bat
@@ -20,8 +20,8 @@ mkdir cmake-x64
 mkdir cmake-x86
 mkdir release-packages
 
-cmake -A x64 -B cmake-x64 %EXTRA_CONFIG_PARAM%
-cmake -A Win32 -B cmake-x86 %EXTRA_CONFIG_PARAM%
+cmake -A x64 -B cmake-x64 %EXTRA_CONFIG_PARAM% -D LIMIT_VERSIONS="1.148.3,1.139.2"
+cmake -A Win32 -B cmake-x86 %EXTRA_CONFIG_PARAM% -D LIMIT_VERSIONS="1.148.3,1.139.2"
 
 cmake --build cmake-x64 --config Release -j %BUILD_JOBS%
 cmake --build cmake-x86 --config Release -j %BUILD_JOBS%
