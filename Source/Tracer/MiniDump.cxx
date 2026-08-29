a8042367edfc8e61934325ecd9999d3380059f07
grasmanek94 <github@gz0.nl>
2026-08-29 21:42:42 +0200

    Fix x86 build

--- Source/Tracer/MiniDump.cxx
+++ Source/Tracer/MiniDump.cxx
@@ -20,44 +20,8 @@ namespace universelan::tracer {
 		CONTEXT ContextRecord;
 		memset(&ContextRecord, 0, sizeof(CONTEXT));
 
-#ifdef _X86_
-
-		__asm {
-			mov dword ptr[ContextRecord.Eax], eax
-			mov dword ptr[ContextRecord.Ecx], ecx
-			mov dword ptr[ContextRecord.Edx], edx
-			mov dword ptr[ContextRecord.Ebx], ebx
-			mov dword ptr[ContextRecord.Esi], esi
-			mov dword ptr[ContextRecord.Edi], edi
-			mov word ptr[ContextRecord.SegSs], ss
-			mov word ptr[ContextRecord.SegCs], cs
-			mov word ptr[ContextRecord.SegDs], ds
-			mov word ptr[ContextRecord.SegEs], es
-			mov word ptr[ContextRecord.SegFs], fs
-			mov word ptr[ContextRecord.SegGs], gs
-			pushfd
-			pop[ContextRecord.EFlags]
-		}
-
-		ContextRecord.ContextFlags = CONTEXT_CONTROL;
-#pragma warning(push)
-#pragma warning(disable:4311)
-		ContextRecord.Eip = (ULONG)_ReturnAddress();
-		ContextRecord.Esp = (ULONG)_AddressOfReturnAddress();
-#pragma warning(pop)
-		ContextRecord.Ebp = *((ULONG*)_AddressOfReturnAddress() - 1);
-
-
-#elif defined (_IA64_) || defined (_AMD64_)
-
 		/* Need to fill up the Context in IA64 and AMD64. */
 		RtlCaptureContext(&ContextRecord);
-
-#else  /* defined (_IA64_) || defined (_AMD64_) */
-
-		ZeroMemory(&ContextRecord, sizeof(ContextRecord));
-
-#endif  /* defined (_IA64_) || defined (_AMD64_) */
 
 		ZeroMemory(&ExceptionRecord, sizeof(EXCEPTION_RECORD));
 
--- Source/Vendor/StackWalker/Main/StackWalker/StackWalker.h
+++ Source/Vendor/StackWalker/Main/StackWalker/StackWalker.h
@@ -1,7 +1,3 @@
-#ifndef _WIN32
-#define __STACKWALKER_H__
-#endif
-
 #ifndef __STACKWALKER_H__
 #define __STACKWALKER_H__
 
@@ -210,46 +206,6 @@ protected:
 //       But I currently use it in x64/IA64 environments...
 //#if defined(_M_IX86) && (_WIN32_WINNT <= 0x0500) && (_MSC_VER < 1400)
 
-#if defined(_M_IX86)
-#ifdef CURRENT_THREAD_VIA_EXCEPTION
-// TODO: The following is not a "good" implementation,
-// because the callstack is only valid in the "__except" block...
-#define GET_CURRENT_CONTEXT_STACKWALKER_CODEPLEX(c, contextFlags)               \
-  do                                                                            \
-  {                                                                             \
-    memset(&c, 0, sizeof(CONTEXT));                                             \
-    EXCEPTION_POINTERS* pExp = NULL;                                            \
-    __try                                                                       \
-    {                                                                           \
-      throw 0;                                                                  \
-    }                                                                           \
-    __except (((pExp = GetExceptionInformation()) ? EXCEPTION_EXECUTE_HANDLER   \
-                                                  : EXCEPTION_EXECUTE_HANDLER)) \
-    {                                                                           \
-    }                                                                           \
-    if (pExp != NULL)                                                           \
-      memcpy(&c, pExp->ContextRecord, sizeof(CONTEXT));                         \
-    c.ContextFlags = contextFlags;                                              \
-  } while (0);
-#else
-// clang-format off
-// The following should be enough for walking the callstack...
-#define GET_CURRENT_CONTEXT_STACKWALKER_CODEPLEX(c, contextFlags) \
-  do                                                              \
-  {                                                               \
-    memset(&c, 0, sizeof(CONTEXT));                               \
-    c.ContextFlags = contextFlags;                                \
-    __asm    call x                                               \
-    __asm x: pop eax                                              \
-    __asm    mov c.Eip, eax                                       \
-    __asm    mov c.Ebp, ebp                                       \
-    __asm    mov c.Esp, esp                                       \
-  } while (0)
-// clang-format on
-#endif
-
-#else
-
 // The following is defined for x86 (XP and higher), x64 and IA64:
 #define GET_CURRENT_CONTEXT_STACKWALKER_CODEPLEX(c, contextFlags) \
   do                                                              \
@@ -258,7 +214,6 @@ protected:
     c.ContextFlags = contextFlags;                                \
     RtlCaptureContext(&c);                                        \
   } while (0);
-#endif
 
 #endif //defined(_WIN32)
 
