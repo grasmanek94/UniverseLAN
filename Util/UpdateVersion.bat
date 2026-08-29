ba09d617924520d6c89390df32c74a1f8a6f9cff
grasmanek94 <github@gz0.nl>
2026-08-29 21:47:58 +0200

    allow version generation if not a git repo

--- Util/UpdateVersion.bat
+++ Util/UpdateVersion.bat
@@ -1,24 +1,37 @@
 @ECHO OFF
+
 cd /d "%~dp0"
 cd ../Source/Version
 
-for /f "delims=" %%a in ('git rev-list HEAD --count') do set _COUNT=%%a
+set "_COUNT="
+
+for /f "delims=" %%a in ('git rev-list HEAD --count 2^>nul') do set "_COUNT=%%a"
+
+if not defined _COUNT (
+    if defined UNIVERSELAN_FORCE_BUILD_NUMBER (
+        set "_COUNT=%UNIVERSELAN_FORCE_BUILD_NUMBER%"
+        echo WARNING: Unable to obtain build number from git, using UNIVERSELAN_FORCE_BUILD_NUMBER=%_COUNT%
+    ) else (
+        set "_COUNT=0"
+        echo WARNING: Unable to obtain build number from git, using build number 0
+    )
+)
+
 echo #define BUILD_VERSION %_COUNT% > auto_version_check.h
 
 fc /b auto_version.h auto_version_check.h > nul
 if errorlevel 1 (
     echo Updating version...
-    copy /b Version.cxx +,,
+    copy /b Version.cxx +,, > nul
     echo touched version.cxx ...
     del auto_version.h
     move auto_version_check.h auto_version.h
     echo Updated version to %_COUNT%
     cd /d %~dp0
-REM exit 0
 ) else (
     del auto_version_check.h
     echo Version doesn't need updating
 )
 
 ver > nul
-type nul>nul
+type nul > nul
--- Util/UpdateVersion.sh
+++ Util/UpdateVersion.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
 cd "$parent_path"
-
 cd ../Source/Version
 
-UPDATE_NR=$(git rev-list HEAD --count)
+if UPDATE_NR=$(git rev-list HEAD --count 2>/dev/null); then
+    :
+else
+    if [ -n "${UNIVERSELAN_FORCE_BUILD_NUMBER}" ]; then
+        UPDATE_NR="${UNIVERSELAN_FORCE_BUILD_NUMBER}"
+        echo "WARNING: Unable to obtain build number from git, using UNIVERSELAN_FORCE_BUILD_NUMBER=${UPDATE_NR}"
+    else
+        UPDATE_NR=0
+        echo "WARNING: Unable to obtain build number from git, using build number 0"
+    fi
+fi
+
 echo "#define BUILD_VERSION ${UPDATE_NR}" > auto_version_check.h
 
 if cmp -s "auto_version.h" "auto_version_check.h"; then
@@ -16,7 +25,7 @@ else
     echo "Updating version..."
     touch Version.cxx
     echo "touched version.cxx ..."
-    rm "auto_version.h"
-    mv "auto_version_check.h" "auto_version.h"
+    rm -f auto_version.h
+    mv auto_version_check.h auto_version.h
     echo "Updated version to ${UPDATE_NR}"
 fi
