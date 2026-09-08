11f44fbf501d6009cc203937e954e093366ce495
grasmanek94 <github@gz0.nl>
2026-09-08 22:49:08 +0200

    Fix const_hash functions bugs, like using wrong seed for 64bit hash and platform agnostic char cast to 64 bits.

--- Source/Shared_NoVer/ConstHash.hxx
+++ Source/Shared_NoVer/ConstHash.hxx
@@ -8,7 +8,7 @@ namespace universelan {
 
 	// compile time FNV-1a
 	inline constexpr uint32_t const_hash(const char* const str, const uint32_t value = 0x811c9dc5) noexcept {
-		return (str[0] == '\0') ? value : const_hash(&str[1], (value ^ uint32_t(str[0])) * 0x1000193);
+		return (str[0] == '\0') ? value : const_hash(&str[1], (value ^ uint32_t(uint8_t(str[0]))) * 0x1000193);
 	}
 
 	inline constexpr uint32_t const_hash(const std::string& str, const uint32_t value = 0x811c9dc5) noexcept {
@@ -16,15 +16,15 @@ namespace universelan {
 	}
 
 	inline constexpr uint64_t const_hash64(const char* const str, const uint64_t value = 0xcbf29ce484222325) noexcept {
-		return (str[0] == '\0') ? value : const_hash64(&str[1], (value ^ uint64_t(str[0])) * 0x100000001b3);
+		return (str[0] == '\0') ? value : const_hash64(&str[1], (value ^ uint64_t(uint8_t(str[0]))) * 0x100000001b3);
 	}
 
-	inline constexpr uint64_t const_hash64(const std::string& str, const uint64_t value = 0x811c9dc5) noexcept {
+	inline constexpr uint64_t const_hash64(const std::string& str, const uint64_t value = 0xcbf29ce484222325) noexcept {
 		return const_hash64(str.c_str(), value);
 	}
 
 	inline constexpr uint64_t const_hash64_data(const char* data, size_t size, const uint64_t value = 0xcbf29ce484222325) noexcept {
-		return (size == 0) ? value : const_hash64_data(&data[1], size - 1, (value ^ uint64_t(data[0])) * 0x100000001b3);
+		return (size == 0) ? value : const_hash64_data(&data[1], size - 1, (value ^ uint64_t(uint8_t(data[0]))) * 0x100000001b3);
 	}
 
 	inline uint64_t const_hash64_data_loop(const char* data, size_t size, uint64_t value = 0xcbf29ce484222325) noexcept {
@@ -33,7 +33,7 @@ namespace universelan {
 		}
 
 		for (size_t i = 0; i < size; ++i) {
-			value ^= uint64_t(data[i]);
+			value ^= uint64_t(uint8_t(data[i]));
 			value *= 0x100000001b3;
 		}
 
