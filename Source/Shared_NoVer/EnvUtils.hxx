e1b5d13938fef1c3af13c6d1da1ae433ffaeeab4
grasmanek94 <github@gz0.nl>
2026-09-11 21:57:57 +0200

    Fix header of envutils for linux regarding wstring change

--- Source/Shared_NoVer/EnvUtils.hxx
+++ Source/Shared_NoVer/EnvUtils.hxx
@@ -5,6 +5,9 @@
 namespace universelan::env_utils
 {
 	std::string get_env(const std::string& var);
+#ifdef _WIN32
 	std::wstring get_env(const std::wstring& var);
+#endif
+
 	std::string get_gamedata_path_prefix();
 }
