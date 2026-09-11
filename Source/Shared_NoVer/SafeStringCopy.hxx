c89a64b59213bf2a3d3ff4503fc92f592007e953
grasmanek94 <github@gz0.nl>
2026-09-11 23:08:01 +0200

    Again lots of safe string fixes.

--- Source/Shared_NoVer/SafeStringCopy.hxx
+++ Source/Shared_NoVer/SafeStringCopy.hxx
@@ -5,108 +5,127 @@
 #include <cstdint>
 #include <cstring>
 #include <format>
+#include <iterator>
 #include <string>
 
 namespace universelan::util {
-	inline uint32_t safe_copy_str_n(const std::string& data, char* const buffer, const uint32_t buffer_len) {
+
+	// Precondition: source and destination ranges must not overlap.
+	// Returns amount of bytes written, including the null terminator.
+	inline uint32_t safe_copy_str_n(const std::string& data, char* const buffer, const uint32_t buffer_len) noexcept {
 		if ((buffer == nullptr) || (buffer_len < 1)) {
 			return 0;
 		}
 
-		uint32_t data_size = (uint32_t)(data.length() + 1);
-		uint32_t copy_size = std::min(data_size, buffer_len);
-		
-		std::copy_n(data.c_str(), copy_size, buffer);
-		if (data_size > buffer_len) {
-			buffer[buffer_len - 1] = '\0';
-		}
-
-		return copy_size;
-	}
-
-	inline uint32_t safe_copy_str_n(const char* const data, char* const buffer, const uint32_t buffer_len) {
-		if ((buffer == nullptr) || (buffer_len < 1)) {
-			return 0;
-		}
-
-		uint32_t data_size = (uint32_t)(std::strlen(data) + 1);
-		uint32_t copy_size = std::min(data_size, buffer_len);
-
-		std::copy_n(data, copy_size, buffer);
-		if (data_size > buffer_len) {
-			buffer[buffer_len - 1] = '\0';
-		}
-
-		return copy_size;
-	}
-
-	inline uint32_t safe_copy_binary_n(const std::string& data, char* const buffer, const uint32_t buffer_len) {
-		if ((buffer == nullptr) || (buffer_len < 1)) {
-			return 0;
-		}
-
-		uint32_t copy_size = std::min((uint32_t)(data.length()), buffer_len);
+		const uint32_t copy_size = (uint32_t)std::min(data.size(), (size_t)(buffer_len - 1U));
 
 		std::copy_n(data.c_str(), copy_size, buffer);
+		buffer[copy_size] = '\0';
 
-		return copy_size;
+		return copy_size + 1;
 	}
 
-	inline uint32_t safe_copy_binary_n(const char* const data, uint32_t data_len, char* const buffer, const uint32_t buffer_len) {
+	// Preconditions:
+	// - data points to a valid null-terminated string.
+	// - source and destination ranges must not overlap.
+	// Returns amount of bytes written, including the null terminator.
+	inline uint32_t safe_copy_str_n(const char* const data, char* const buffer, const uint32_t buffer_len) noexcept {
+		if ((data == nullptr) || (buffer == nullptr) || (buffer_len < 1)) {
+			return 0;
+		}
+
+		const uint32_t copy_size = (uint32_t)std::min(std::strlen(data), (size_t)(buffer_len - 1U));
+
+		std::copy_n(data, copy_size, buffer);
+		buffer[copy_size] = '\0';
+
+		return copy_size + 1;
+	}
+
+	// Precondition: source and destination ranges must not overlap.
+	// Returns amount of bytes written.
+	inline uint32_t safe_copy_binary_n(const std::string& data, char* const buffer, const uint32_t buffer_len) noexcept {
 		if ((buffer == nullptr) || (buffer_len < 1)) {
 			return 0;
 		}
 
-		uint32_t copy_size = std::min((uint32_t)(data_len), buffer_len);
+		const uint32_t copy_size = (uint32_t)std::min(data.length(), (size_t)buffer_len);
+
+		std::copy_n(data.data(), copy_size, buffer);
+
+		return copy_size;
+	}
+
+	// Precondition: source and destination ranges must not overlap.
+	// Returns amount of bytes written.
+	inline uint32_t safe_copy_binary_n(const char* const data, uint32_t data_len, char* const buffer, const uint32_t buffer_len) noexcept {
+		if ((data == nullptr) || (buffer == nullptr) || (buffer_len < 1)) {
+			return 0;
+		}
+
+		const uint32_t copy_size = std::min(data_len, buffer_len);
 
 		std::copy_n(data, copy_size, buffer);
 
 		return copy_size;
 	}
 
-	inline const char* safe_fix_null_char_ptr(const char*& value) {
+	inline const char* safe_fix_null_char_ptr(const char*& value) noexcept {
 		if (value == nullptr) {
 			value = "";
 		}
 		return value;
 	}
 
-	inline const char* safe_fix_null_char_ptr_ret(const char* const value) {
+	inline const char* safe_fix_null_char_ptr_ret(const char* const value) noexcept {
 		if (value == nullptr) {
 			return "";
 		}
 		return value;
 	}
 
-	inline const char* safe_fix_null_char_ptr_annotate_ret(const char* const value) {
+	inline const char* safe_fix_null_char_ptr_annotate_ret(const char* const value) noexcept {
 		if (value == nullptr) {
 			return "!!(nullptr)";
 		}
 		return value;
 	}
 
-#ifndef _WIN32
-	inline size_t strnlen_s(const char* const str, const size_t strsz) {
-		if (str == nullptr) {
+	inline size_t bounded_strlen(const char* const str, const size_t maximum) noexcept
+	{
+		if (str == nullptr || maximum == 0) {
 			return 0;
 		}
-		return strnlen(str, strsz);
+
+		const void* const terminator =
+			std::memchr(str, '\0', maximum);
+
+		if (terminator == nullptr) {
+			return maximum;
+		}
+
+		return (size_t)(static_cast<const char*>(terminator) - str);
 	}
 
-#endif
-
-	inline std::string safe_fix_null_char_ptr_annotate(const char* buffer, uint32_t buffer_len) {
+	inline std::string safe_fix_null_char_ptr_annotate(const char* const buffer, uint32_t buffer_len) {
 		if (buffer == nullptr) {
 			return "!!(nullptr)";
 		}
-		return std::string(buffer, std::min((size_t)buffer_len, strnlen_s(buffer, buffer_len)));
+		return std::string(buffer, bounded_strlen(buffer, (size_t)buffer_len));
 	}
 
 	inline std::string bytes_to_hex(const void* const data, const uint32_t dataSize) {
-		std::string hex;
-		for (uint32_t i = 0; i < dataSize; ++i) {
-			hex += std::format("{:02x}", ((const unsigned char*)data)[i]);
+		if (data == nullptr) {
+			return "";
 		}
+
+		std::string hex{};
+		hex.reserve((size_t)(dataSize) * 2U);
+
+		for (uint32_t i = 0; i < dataSize; ++i) {
+			std::format_to(std::back_inserter(hex), "{:02x}", ((const unsigned char*)data)[i]);
+		}
+
 		return hex;
 	}
 }
