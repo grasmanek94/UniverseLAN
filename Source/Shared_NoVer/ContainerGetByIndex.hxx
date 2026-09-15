f7c528e9fa171d4932d1b04a2b1df59d51b2320f
grasmanek94 <github@gz0.nl>
2026-09-15 21:37:23 +0200

    add new container functionality to get by index

--- Source/Shared_NoVer/ContainerGetByIndex.hxx
+++ Source/Shared_NoVer/ContainerGetByIndex.hxx
@@ -19,4 +19,47 @@ namespace universelan {
 
 		return default_not_found_value;
 	}
+
+    template <typename Container>
+    auto container_iterator_get_by_index(
+        Container& container,
+        std::size_t index)
+    {
+        if (index >= container.size()) {
+            return container.end();
+        }
+
+        return std::next(container.begin(), index);
+    }
+
+    template <typename Map>
+    auto map_iterator_get_by_index_with_forced_zero_key(
+        Map& map,
+        std::size_t index,
+        const typename Map::key_type& forced_zero_key)
+    {
+        if (index >= map.size()) {
+            return map.end();
+        }
+
+        if (index == 0) {
+            return map.find(forced_zero_key);
+        }
+
+        std::size_t virtual_index = 1;
+
+        for (auto it = map.begin(); it != map.end(); ++it) {
+            if (it->first == forced_zero_key) {
+                continue;
+            }
+
+            if (virtual_index == index) {
+                return it;
+            }
+
+            ++virtual_index;
+        }
+
+        return map.end();
+    }
 }
