08f053e0dd19b11d1a54962997828878616b8a88
grasmanek94 <github@gz0.nl>
2026-09-20 22:00:06 +0200

    filesystem container don't separately lock twice in a single function

--- Source/Shared_NoVer/filesystem_container/filesystem_container.cxx
+++ Source/Shared_NoVer/filesystem_container/filesystem_container.cxx
@@ -260,13 +260,6 @@ namespace filesystem_container {
 			return;
 		}
 
-		{
-			lock_t lock{ mtx_filename_to_entry };
-			if (filename_to_entry.find(entry->get_path()) != filename_to_entry.end()) {
-				return;
-			}
-		}
-
 		std::unique_lock<mutex_t> lk1(mtx_fs_entry_index, std::defer_lock);
 		std::unique_lock<mutex_t> lk2(mtx_filename_to_entry, std::defer_lock);
 		std::unique_lock<mutex_t> lk3(mtx_shareid_to_entry, std::defer_lock);
@@ -276,6 +269,10 @@ namespace filesystem_container {
 		}
 		else {
 			std::lock(lk1, lk2);
+		}
+
+		if (filename_to_entry.find(entry->get_path()) != filename_to_entry.end()) {
+			return;
 		}
 
 		filename_to_entry.emplace(entry->get_path(), entry);
