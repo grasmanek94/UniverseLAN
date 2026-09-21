fa0b10513c6d313b2f29201aa2a4bcbb3f00feaa
grasmanek94 <github@gz0.nl>
2026-09-21 23:13:23 +0200

    Proxy listener fixes

--- Source/InterceptionLogger/Impl/Listeners/ProxifySingleShotListener.hxx
+++ Source/InterceptionLogger/Impl/Listeners/ProxifySingleShotListener.hxx
@@ -1,9 +1,11 @@
 #pragma once
 #include <IListenerRegistrar.h>
 
+#include <functional>
 #include <mutex>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 class ProxifySyncHandler {
 public:
@@ -21,21 +23,23 @@ private:
 
 public:
 	static ProxifySyncHandler* get() {
-		static ProxifySyncHandler instance;
+		static ProxifySyncHandler instance{};
 		return &instance;
 	}
 
-	static void add(galaxy::api::IGalaxyListener* real, galaxy::api::IGalaxyListener* proxy);
-	static galaxy::api::IGalaxyListener* pop(galaxy::api::IGalaxyListener* real);
+	static void add(galaxy::api::IGalaxyListener* proxy, galaxy::api::IGalaxyListener* real);
+	static std::vector<galaxy::api::IGalaxyListener*> pop_proxies_from_real(galaxy::api::IGalaxyListener* real);
+	static galaxy::api::IGalaxyListener* pop_real_from_proxy(galaxy::api::IGalaxyListener* proxy);
 
 	static void delete_protection_enable(galaxy::api::IGalaxyListener* proxy);
 	static bool is_delete_protection_enabled(galaxy::api::IGalaxyListener* proxy);
+	static bool delete_if_not_protected(galaxy::api::IGalaxyListener* proxy);
 	static void delete_protection_disable(galaxy::api::IGalaxyListener* proxy);
 };
 
 #define IMPLEMENT_PROXY_ENCAPSULATE_FUNC_FOR(class_type, listener_type) \
 public: \
-	inline class_type() : target{nullptr}, proxify_unregister_delete_protection{false} {} \
+	inline class_type() : target{nullptr} {} \
 	static inline listener_type* encapsulate(listener_type* listener) \
 	{ \
 		if (!listener) { \
@@ -43,19 +47,22 @@ public: \
 		} \
 		class_type* proxy = new class_type(); \
 		proxy->target = listener; \
-		ProxifySyncHandler::add(proxy->target, proxy); \
+		ProxifySyncHandler::add(proxy, proxy->target); \
 		return proxy; \
 	} \
 private: \
-	listener_type* target; \
-	bool proxify_unregister_delete_protection
+	listener_type* target
 
 #define IMPLEMENT_PROXY_CALL_ORIGINAL_LISTENER_FUNC(func) \
-	if (target) { \
-		trace.write_all("listener: {}", (void*)target); \
-		ProxifySyncHandler::delete_protection_enable(this); \
-		target->func; \
-		ProxifySyncHandler::delete_protection_disable(this); \
-		ProxifySyncHandler::pop(target); \
-		delete this; \
-	}
+	do { /* Explicit contract that IMPLEMENT_PROXY_CALL_ORIGINAL_LISTENER_FUNC MUST be the LAST instruction in a callback */ \
+		if (target) { \
+			trace.write_all("listener: {}", (void*)target); \
+			ProxifySyncHandler::delete_protection_enable(this); \
+			target->func; \
+			ProxifySyncHandler::delete_protection_disable(this); \
+			ProxifySyncHandler::pop_real_from_proxy(this); \
+			delete this; \
+		} \
+		return; \
+	} \
+	while(false)
